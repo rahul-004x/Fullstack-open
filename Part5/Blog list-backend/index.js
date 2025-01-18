0798690f9a98d083f0068dbf2f5189e271fed125
rahul-004x <142470408+rahul-004x@users.noreply.github.com>
2025-01-18 14:41:09 +0530

    Removed cached files

--- Part5/Blog list-backend/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
--- Part7/bloglist-frontend-rq-ctx/.eslintrc.cjs
+++
@@ -1,1 +0,0 @@
-export const extends = ["plugin:storybook/recommended"];
